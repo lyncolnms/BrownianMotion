1d887db4ad3c697dc4f4cc16f69547656d97f894
lyncolnmauricio <lyncolnms@gmail.com>
2025-09-04 00:38:42 -0300

    add BrownianGraphicPageViewModel for managing Brownian Motion parameters

---
+++ BrownianMotion/Features/BrownianGraphic/BrownianGraphicPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BrownianMotion.Features.BrownianGraphic.Drawables;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BrownianMotion.Features.BrownianGraphic;
+
+public partial class BrownianGraphicPageViewModel : ObservableObject
+{
+    [ObservableProperty] private IDrawable _brownianMotionDrawable;
+    [ObservableProperty] private double _sigma = 20;
+    [ObservableProperty] private double _mean = 1;
+    [ObservableProperty] private double _initialPrice = 100;
+    [ObservableProperty] private int _numDays = 252;
+
+    [RelayCommand]
+    private async Task GenerateBrownianMotion()
+    {
+        BrownianMotionDrawable = new BrownianMotionDrawable(Sigma/100, Mean/100, InitialPrice, NumDays);
+    }
+}
