64014398df3c444d8c67fb54422d896bb66d619e
lyncolnmauricio <lyncolnms@gmail.com>
2025-09-04 00:37:42 -0300

    add BrownianMotion feature integration to Maui application

--- BrownianMotion/MauiProgram.cs
+++ BrownianMotion/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Maui;
+﻿using BrownianMotion.Features.BrownianGraphic;
+using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
 
 namespace BrownianMotion;
@@ -17,10 +18,23 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+        builder.Services.AddPages<BrownianGraphicPage, BrownianGraphicPageViewModel>();
+
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
 
         return builder.Build();
+    }
+
+    private static void AddPages<TPage, TPageViewModel>(this IServiceCollection services)
+        where TPage : ContentPage, new() 
+        where TPageViewModel : class
+    {
+        services.AddTransient<TPageViewModel>();
+        services.AddTransient<TPage>(serviceProvider => new TPage
+        {
+            BindingContext = serviceProvider.GetRequiredService<TPageViewModel>()
+        });
     }
 }
