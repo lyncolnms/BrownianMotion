f0f7eaab6ddb53db3016c986931a463a42e4cac8
lyncolnmauricio <lyncolnms@gmail.com>
2025-09-05 11:56:12 -0300

    add ColorsHelper for generating random colors in Brownian Motion visualizations

---
+++ BrownianMotion/Helpers/Colors/ColorsHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace BrownianMotion.Helpers.Colors;
+
+public static class ColorsHelper
+{
+    public static Color GetRandomColor()
+    {
+        Random rnd = new();
+
+        int r = rnd.Next(50, 206);
+        int g = rnd.Next(50, 206);
+        int b = rnd.Next(50, 206);
+
+        return Color.FromRgb(r, g, b);
+    }
+}
