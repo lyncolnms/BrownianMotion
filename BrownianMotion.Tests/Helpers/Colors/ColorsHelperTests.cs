9b5814a03613a6391e1e52504aac2480be8d6408
lyncolnmauricio <lyncolnms@gmail.com>
2025-09-05 15:16:22 -0300

    add ColorsHelperTests to validate random color generation

---
+++ BrownianMotion.Tests/Helpers/Colors/ColorsHelperTests.cs
@@ -0,0 +1,18 @@
+﻿using BrownianMotion.Helpers.Colors;
+using FluentAssertions;
+
+namespace BrownianMotion.Tests.Helpers.Colors;
+
+[TestFixture]
+public class ColorsHelperTests
+{
+    [Test]
+    public void ShouldReturnRandomColor()
+    {
+        ColorsHelper.GetRandomColor().ToRgb(out byte red, out byte green, out byte blue);
+
+        red.Should().BeInRange(50, 205);
+        green.Should().BeInRange(50, 205);
+        blue.Should().BeInRange(50, 205);
+    }
+}
